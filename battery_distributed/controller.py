31e4f28238f53548b8fd5eebb8af0459f4e93bde
Thiago Paiva <thiagohdaqw@gmail.com>
2023-05-21 22:06:24 -0300

    adicionando mock da leitura do controller

--- battery_distributed/controller.py
+++ battery_distributed/controller.py
@@ -1,8 +1,9 @@
 import os
 import time
+import random
 import logging
 
-from threading import Thread
+from threading import Thread, Semaphore
 from subprocess import Popen, PIPE
 
 from battery_distributed.model import Maquina
@@ -11,17 +12,24 @@ LOG = "Controller"
 CONTROLLER_RUNNER_PATH=os.environ.get("CONTROLLER_RUNNER_PATH", "tests/controller_mock.sh")
 
 
-def init(maquina: Maquina):
-    Thread(target=controller_spawner).start()
+def init(maquina: Maquina, analyser_sem: Semaphore):
+    Thread(target=controller_spawner, args=(maquina, analyser_sem)).start()
 
 
-def controller_spawner():
+def controller_spawner(maquina: Maquina, analyser_sem: Semaphore):
     while True:
         try:
             logging.info(f"{LOG}: Spawning {CONTROLLER_RUNNER_PATH} controller")
             with Popen([CONTROLLER_RUNNER_PATH], stdout=PIPE, stdin=PIPE) as proc:
                 for line in proc.stdout:
                     logging.debug(f"{LOG}: Receiving from controller: {line}")
+                    # Mock
+                    analyser_sem.release()
+                    maquina.quantidade_AA = random.randint(0, 20)
+                    maquina.quantidade_AAA = random.randint(0, 20)
+                    maquina.quantidade_C = random.randint(0, 20)
+                    maquina.quantidade_D = random.randint(0, 20)
+                    maquina.quantidade_V9 = random.randint(0, 20)
 
         except Exception as e:
             logging.error(f"{LOG}: Error in controller runner. Respawnning in 5 seconds... {e}")
