04ee99208ad62c2c29c32c38e9d49840b7fcad20
Thiago Paiva <thiagohdaqw@gmail.com>
2023-05-26 01:13:20 -0300

    Implementando o picamera

--- battery_distributed/analyser.py
+++ battery_distributed/analyser.py
@@ -1,6 +1,8 @@
 import os
-import cv2
 import logging
+import picamera
+import numpy as np
+import time
 
 from threading import Thread, Semaphore
 
@@ -8,6 +10,7 @@ from battery_analyser import predict as ba_predict
 
 LOG = "Analyser"
 CAM_ID = int(os.environ.get("CAM_ID", "0"))
+MAX_TRIES = int(os.environ.get("ANALYSER_MAX_TRIES", "10"))
 
 
 def init() -> Semaphore:
@@ -17,13 +20,22 @@ def init() -> Semaphore:
 
 
 def run_analyser_worker(sem: Semaphore):
-    cam = cv2.VideoCapture(CAM_ID)
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH, ba_predict.WIDTH)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, ba_predict.HEIGHT)
+    with picamera.PiCamera() as cam:
+        cam.resolution = (640, 480)
+        cam.start_preview()
+#        cam.color_effects=(128,128)
+        output = np.empty((ba_predict.WIDTH, ba_predict.HEIGHT, 3), dtype=np.uint8)
+        while True:
+            time.sleep(0.5)
+            for try_ in range(MAX_TRIES):
+                cam.capture(output, 'rgb', resize=ba_predict.IMAGE_SIZE)
+                cam.capture('imagem.jpg', resize=ba_predict.IMAGE_SIZE)
 
-    while True:
-        sem.acquire()
-        _, frame = cam.read()
-
-        prediction = ba_predict.predict(frame)
-        logging.info(f"{LOG}: {prediction}")
+                prediction = ba_predict.predict(output)
+                if not prediction:
+                    if try_+1 == MAX_TRIES:
+                        logging.error(f"{LOG}: max atempt reached...")
+                        break
+                    logging.warn(f"{LOG}: cant predict. Trying again...")
+                    continue
+                logging.info(f"{LOG}: {prediction}")
